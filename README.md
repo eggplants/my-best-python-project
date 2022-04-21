86c5e3d2a7ee3b3ab728039dfaa365bf78fd16a8
eggplants <w10776e8w@yahoo.co.jp>
2022-04-21 21:35:32 +0900

    add: cli

--- README.md
+++ README.md
@@ -8,6 +8,10 @@
   https://api.codeclimate.com/v1/badges/e6d94059d1dc7f08d2a4/maintainability
   )](
   https://codeclimate.com/github/eggplants/my-best-python-project/maintainability
+) [![Release Package](
+  https://github.com/eggplants/my-best-python-project/actions/workflows/release.yml/badge.svg
+  )](
+  https://github.com/eggplants/my-best-python-project/actions/workflows/release.yml
 )
 
 [![pre-commit.ci status](
@@ -28,6 +32,69 @@ This is a sample project.
 pip install git+https://github.com/eggplants/my-best-python-project
 # or,
 pip install my-best-python-project
+```
+
+## Usage
+
+### CLI
+
+```shellsession
+$ mbpp -h
+usage: mbpp [-h] [-o PATH] [--overwrite] [-q] [-V]
+
+This command prints package's version.
+
+optional arguments:
+  -h, --help              show this help message and exit
+  -o PATH, --output PATH  output to file (default: None)
+  --overwrite             overwrite when using `-o` (default: False)
+  -q, --quiet             quiet mode (default: False)
+  -V, --version           show program's version number and exit
+
+note:
+    This package and tool is a sample.
+
+$ mbpp
+This package's version is: 0.0.1
+
+$ mbpp -q
+0.0.1
+
+$ mbpp -o test.txt
+Output: 'test.txt'
+
+$ mbpp -o test.txt
+Error: File 'test.txt' exists. To overwrite, use `-o`.
+
+$ mbpp -o test.txt --overwrite
+Output: 'test.txt'
+```
+
+### Library
+
+To print this package's version:
+
+```python
+import my_best_python_project
+
+print(my_best_python_project.__version__)
+```
+
+## Development
+
+To setup development environment:
+
+```sh
+pip install -e ".[all]"
+pre-commit install
+```
+
+To run pre-commit hooks manually:
+
+```sh
+pre-commit run
+# or,
+pre-commit run --all-fileso
 ```
 
 ## License
---
+++ my_best_python_project/main.py
@@ -0,0 +1,90 @@
+from __future__ import annotations
+
+import argparse
+import os
+import shutil
+import sys
+import textwrap
+from typing import Optional, cast
+
+from . import __version__
+
+
+class MBBPHelpFormatter(
+    argparse.ArgumentDefaultsHelpFormatter, argparse.RawDescriptionHelpFormatter
+):
+    pass
+
+
+def check_out(s: str | None) -> str | None:
+    if s is None:
+        return None
+    elif os.path.isdir(s):
+        raise argparse.ArgumentTypeError(f"{repr(s)} is a dir.")
+    else:
+        return s
+
+
+def parse_args() -> argparse.Namespace:
+    usage = textwrap.dedent(
+        """
+    note:
+        This package and tool is a sample.
+    """
+    )
+
+    parser = argparse.ArgumentParser(
+        prog="mbpp",
+        description="This command prints package's version.",
+        formatter_class=(
+            lambda prog: MBBPHelpFormatter(
+                prog,
+                **{
+                    "width": shutil.get_terminal_size(fallback=(120, 50)).columns,
+                    "max_help_position": 40,
+                },
+            )
+        ),
+        epilog=usage,
+    )
+    parser.add_argument(
+        "-o", "--output", type=check_out, help="output to file", metavar="PATH"
+    )
+    parser.add_argument(
+        "--overwrite", action="store_true", help="overwrite when using `-o`"
+    )
+    parser.add_argument("-q", "--quiet", action="store_true", help="quiet mode")
+    parser.add_argument(
+        "-V", "--version", action="version", version=f"%(prog)s {__version__}"
+    )
+    return parser.parse_args()
+
+
+def main() -> None:
+    args = parse_args()
+    quiet = cast(bool, args.quiet)
+    overwrite = cast(bool, args.overwrite)
+    output_path = cast(Optional[str], args.output)
+
+    contents = []
+    if not quiet:
+        contents.append("This package's version is:")
+    contents.append(__version__)
+    content = " ".join(contents)
+
+    if output_path is None:
+        print(content)
+    elif not os.path.isfile(output_path) or overwrite:
+        print(content, file=open(output_path, "w"))
+        if not quiet:
+            print(f"Output: {repr(output_path)}", file=sys.stderr)
+    else:
+        print(
+            f"Error: File {repr(output_path)} exists. To overwrite, use `-o`.",
+            file=sys.stderr,
+        )
+        exit(1)
+
+
+if __name__ == "__main__":
+    main()
